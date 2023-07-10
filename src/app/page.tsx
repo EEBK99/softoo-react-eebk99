cebb5bf80d25b5344ec9a766587e5ce7a2f9abe8
Haseeb Khan <haseeb999khan@gmail.com>
2023-07-10 22:05:24 +0500

    API data structuring for chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <div>Home page</div>;
+import { ChartContent } from "./chart";
+
+async function getData() {
+  const res = await fetch(
+    "http://api.thunder.softoo.co/vis/api/dashboard/ssu/fixed"
+  );
+  const data = await res.json();
+  return data;
+}
+
+export default async function Home() {
+  const data = await getData();
+  return (
+    <div>
+      <ChartContent apiData={data} />
+    </div>
+  );
 }
