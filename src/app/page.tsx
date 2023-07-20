821a21a71dee1c3e517233ac9d0c6fd03928ad4d
Haseeb Khan <haseeb999khan@gmail.com>
2023-07-20 17:44:00 +0500

    API data structuring for chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ import { ChartContent } from "./chart";
 
 async function getData() {
   const res = await fetch(
-    "http://api.thunder.softoo.co/vis/api/dashboard/ssu/fixed"
+    "http://api.thunder.softoo.co/vis/api/dashboard/ssu/fixed",
+    { cache: "no-store" }
   );
   const data = await res.json();
   return data;
